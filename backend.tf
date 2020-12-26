ac397e7e4c7bf052ba48bfc400247f216e69ba63
Asghar Ghori <40242692+aghori1@users.noreply.github.com>
2020-12-26 14:41:25 -0500

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "tfstates-RG"
-    storage_account_name = "tfstatesasghar"
-    container_name       = "tfstatefiles"
-    key                  = "terraformmodulestate"
-  }
-}
